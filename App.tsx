bb5c9cec60ac13c78aacedb4f7a434c9b5ecc74f
alexlecco <alexlecco24@gmail.com>
2019-10-13 21:35:50 -0300

    connect with movie api

--- App.tsx
+++ App.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props: Readonly<{}>) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: null,
+    }
+  }
+
+  componentDidMount() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((res) => res.json())
+      .then((resJson) => {
+        this.setState({
+          dataSource: resJson.movies,
+          isLoading: false,
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    console.log("dataSource::::", this.state.dataSource)
+    if(this.state.isLoading) {
+      return(
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      )
+    } else {
+        let movies = this.state.dataSource.map((val, key) => {
+          return(
+            <View key={key} style={styles.item}>
+              <Text>{val.title}</Text>
+            </View>
+          )
+        })
+
+        return (
+          <View style={styles.container}>
+            {movies}
+          </View>
+        )
+    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +57,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item: {
+    flex: 1,
+    alignSelf: 'stretch',
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  }
 });
